0344377e93722aed088a4c53f45b9ee04e0af311
EddieFontenele <70458978+EddieFontenele@users.noreply.github.com>
2024-07-29 12:59:10 -0300

    Config Dev fix
    Public folder created

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,7 +1,7 @@
-const { merge } = require('webpack-merge')
-const path = require('path')
+const path = require('path');
+const { merge } = require('webpack-merge');
 
-const config = require('./webpack.config')
+const config = require('./webpack.config');
 
 module.exports = merge(config, {
     mode: 'development',
@@ -9,10 +9,14 @@ module.exports = merge(config, {
     devtool: 'inline-source-map',
 
     devServer: {
-        writeToDisk: true
+        devMiddleware: {
+            writeToDisk: true,
+        },
     },
 
     output: {
-        path: path.resolve(__dirname, 'public')
-    }
-})
+        path: path.resolve(__dirname, 'public'),
+        assetModuleFilename: '[name][ext]',
+        clean: true,
+    },
+});
