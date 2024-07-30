2a8b524fb0f860334aea1ecd4f722dc6ea1ff636
EddieFontenele <70458978+EddieFontenele@users.noreply.github.com>
2024-07-29 23:47:58 -0300

    Wroking after passing meta data
    Pub working index!

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express()
 const path = require('path')
 const port = 3000
 
-app.engine('pug', require('pug').__express)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    meta: {
+      data: {
+        title: 'Floema',
+        description: 'Metadata description.'
+      }
+    }
+  })
 })
 
 app.listen(port, () => {
